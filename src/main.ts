296cbd5c5b11e8e22d34e848bea5d3f5369b1721
Vince Coppens <vince.coppens@gmail.com>
2026-03-18 22:42:36 +0100

    Analytics

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { inject } from '@vercel/analytics'
+
+inject()
 
 const app = createApp(App)
 app.use(router)
