1fb2f8a481e54685688ad093b071f0db3f24e0df
Vince Coppens <vince.coppens@gmail.com>
2026-03-19 13:17:32 +0100

    sped insights

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 
 inject()
+injectSpeedInsights()
 
 const app = createApp(App)
 app.use(router)
